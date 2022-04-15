3e7bc162ac70b7de0b331a2a741e0b0dce2289bd
Andrei Zavada <andrei.zavada@tiot.jp>
2022-04-16 01:29:05 +0300

    rm riak_test/ (now incorporated to riak_test proper)

--- riak_test/intercepts/intercept.hrl
+++
@@ -1,4 +0,0 @@
-%% Copied from riak_test
--define(I_TAG(S), "INTERCEPT: " ++ S).
--define(I_INFO(Msg), error_logger:info_msg(?I_TAG(Msg))).
--define(I_INFO(Msg, Args), error_logger:info_msg(?I_TAG(Msg), Args)).
